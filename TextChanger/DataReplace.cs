45b00db10dbace70269dd5ac3ee6631f40967f23
MrChew <haronkg@gmail.com>
2014-06-30 14:53:27 +0400

    Add Class "DataReplace".
    Add class and rewrite main program.

---
+++ TextChanger/DataReplace.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextChanger
+{
+    public class DataReplace
+    {
+         private string _content="";
+        private int _ProcessedFiles = 0;
+        private int _FoundFiles=0;
+
+
+        public int ReplaceTextInFile(string pattern, string newValue, string path)
+        {
+            Regex RegText = new Regex(pattern, RegexOptions.ExplicitCapture);
+            try
+            {
+                using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        _content = reader.ReadToEnd();
+                    }
+                }
+                // Counting matching in file
+                MatchCollection MatchesCount = RegText.Matches(_content);
+                // Replacing %pattern% to %NewValue%
+                if (MatchesCount.Count != 0)
+                {
+                    _content = RegText.Replace(_content, newValue);
+                    // Open stream for writing data into file
+                    using (StreamWriter writer = new StreamWriter(path))
+                    {
+                        writer.Write(_content);
+                    }
+                    _ProcessedFiles++;
+                    return MatchesCount.Count;
+                    //Console.WriteLine("{0}. File <{1}>: subtituted {2} entries.", i, Path, MatchesCount.Count);
+                    //ProcessedFiles++;
+                }
+                else
+                {
+                    Console.WriteLine("File <{0}> no matches found.",path);
+                    return (0);
+                }
+            }
+
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File <{0}> not found", path);
+                _FoundFiles++;
+                return (0);
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("File <{0}> access denied. Please run programm as Administrator",path);
+                return (0);
+            }
+
+            catch (FileLoadException e)
+            {
+                Console.WriteLine("File <{0}> access denied",path);
+                return (0);
+            }
+        }
+
+        public void Summary(int i)
+        {
+        Console.WriteLine("Specified: {0} files; Found: {1} files; Processed: {2} files", i, _FoundFiles, _ProcessedFiles);
+        return;
+        }
+
+        public int FoundFiles()
+        {
+            return _FoundFiles;
+        }
+        public int ProcessedFiles()
+        {
+            return _ProcessedFiles;
+        }
+    }
+}
--- TextChanger/Tch.cs
+++ TextChanger/Tch.cs
@@ -21,74 +21,24 @@ namespace TextChanger
                 Environment.Exit(0);
             }
 
-           string OldValue = (string)(args[0]);
+           string Pattern = (string)(args[0]);
            string NewValue = (string)(args[1]);
 
             //Reading pathes to files
             string[] Pathes = ConfigurationManager.AppSettings["Path"].Split(';');
-
-            //Create regular 
-            Regex RegText = new Regex(OldValue, RegexOptions.ExplicitCapture);
-
-            //V
-            int SpecifiedFiles = Pathes.Length;
-            int FoundFiles = SpecifiedFiles;
-            int ProcessedFiles = 0;
-            string content="";
-            int i = 0;
-
-            foreach (string Path in Pathes)
+           
+           DataReplace DR = new DataReplace();
+           int CountSubtitud=0;
+           foreach (string Path in Pathes)
             {
-                i++;
-                try
+                CountSubtitud = DR.ReplaceTextInFile(Pattern, NewValue, Path);
+                if (CountSubtitud != 0)
                 {
-                    using (FileStream file = new FileStream(Path, FileMode.Open, FileAccess.ReadWrite))
-                    { 
-                        using (StreamReader reader = new StreamReader(file))
-                            {
-                             content= reader.ReadToEnd();
-                            }
-                    }
-                    // Counting matching in file
-                    MatchCollection MatchesCount = RegText.Matches(content);
-                    // Replacing %OldValue% to %NewValue%
-                    if (MatchesCount.Count != 0)
-                    {
-                        content = RegText.Replace(content, NewValue);
-                        // Open stream for writing data into file
-                        using (StreamWriter writer = new StreamWriter(Path))
-                        {
-                            writer.Write(content);
-                        }
-                        Console.WriteLine("{0}. File <{1}>: subtituted {2} entries.", i, Path, MatchesCount.Count);
-                        ProcessedFiles++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0}.File <{1}> no matches found.", i, Path);
-                    }
+                    Console.WriteLine("File <{0}>: subtituted {1} entries.",Path, CountSubtitud);
                 }
-                    
-                catch (FileNotFoundException e)
-                {
-                    Console.WriteLine("{0}. File <{1}> not found", i, Path);
-                    FoundFiles--;
-                }
-
-                catch (UnauthorizedAccessException e)
-                {
-                    Console.WriteLine("{0}. File <{1}> access denied. Please run programm as Administrator", i, Path);
-                }
-
-                catch (FileLoadException e)
-                {
-                    Console.WriteLine("{0}. File <{1}> access denied", i, Path);
-                }
-                
             }
             Console.WriteLine();
-            Console.WriteLine("---Summary---");
-            Console.WriteLine("Specified: {0} files; Found: {1} files; Processed: {2} files",SpecifiedFiles,FoundFiles,ProcessedFiles);
+            DR.Summary(Pathes.Length);
             //Console.ReadKey();
         }
     }
